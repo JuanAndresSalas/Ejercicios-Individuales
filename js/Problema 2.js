bb5251d253a47e099c97ebd6d450a8e7a20c82b8
Juan Salas <juan.salas.salas@outlook.com>
2023-01-28 03:20:09 -0300

    Problema 2 terminado

---
+++ js/Problema 2.js
@@ -0,0 +1,61 @@
+$("#btnFormulario").on('click', function(){
+    let edad = $("#inputEdad").val();
+    let cedula = $('input:radio[name=cedula]:checked').val();
+    let ahorro = $("#inputAhorro").val();
+    let rsh = $("#rsh").val();
+    let nucleoFamiliar = $("#inputNucleoFamiliar").val();
+    let ingresoFamiliar = $("#inputIngresoFamiliar").val();
+    let minvu = $('input:radio[name=minvu]:checked').val();
+    let criterios = [];
+    
+    if(edad > 18 && edad <= 60 && nucleoFamiliar >= 2){
+        criterios.push(true)
+    }else if(edad < 18 || edad < 60 && nucleoFamiliar <= 1){
+        criterios.push(false)
+    }
+
+    switch (cedula) {
+        case "si":
+            criterios.push(true)
+            break;
+    
+        case "no":
+            criterios.push(false)
+            break;
+    }
+
+    if(ahorro < 4){
+        criterios.push(false)
+    }else{
+        criterios.push(true)
+    }
+    
+    if(rsh > 70){
+        criterios.push(false)
+    }else{
+        criterios.push(true)
+    }
+    
+    if(ingresoFamiliar >= 7 && ingresoFamiliar <= 25){
+        criterios.push(true)
+    }else{
+        criterios.push(false)
+    }
+
+    switch (minvu) {
+        case "si":
+            criterios.push(false)
+            break;
+    
+        case "no":
+            criterios.push(true)
+            break;
+    }
+
+    if(criterios.indexOf(false) > -1 ){
+        alert("No cumple con todos los requisitos" )
+    }else {
+        alert("Cumple con los requisitos")
+    }
+})
+
